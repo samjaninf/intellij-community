945733f6516a8a24dbd67c92d11f8f11b3b889c8
Ilia Kirianovskii <Ilia.Kirianovskii@jetbrains.com>
2026-01-22 22:48:34 +0100

    [build] Move LoggingRule to com.jetbrains.env package to avoid sealing violation w/ jbr-api library (IJI-3451)
    GitOrigin-RevId: fc9215a2f1d0cddf1a0bf67f083aaf2cf9816eb4

--- python/testSrc/com/jetbrains/env/LoggingRule.kt
+++ python/testSrc/com/jetbrains/env/LoggingRule.kt
@@ -1,5 +1,5 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.jetbrains
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.jetbrains.env
 
 import com.intellij.openapi.Disposable
 import com.intellij.testFramework.TestLoggerFactory
