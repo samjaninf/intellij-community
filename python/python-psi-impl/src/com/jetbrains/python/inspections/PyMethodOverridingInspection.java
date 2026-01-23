0a4abe2e79585f7840d39f2d70ab6a705304907d
Daniil Kalinin <Daniil.Kalinin@jetbrains.com>
2026-01-23 16:14:50 +0100

    PY-87372 Extend PyMethodOverridingInspection to report mismatched types in signatures and returns
    - Refactor, remove obsolete API usages
    - Add a few tests
    
    Space-RevId: e5a70d23d902170e4bbde16f1dacd4349c7078b1
    
    GitOrigin-RevId: 46e0a3a6aa0cb7d9b2101f7895ed23d014b829f4

--- python/python-psi-impl/src/com/jetbrains/python/inspections/PyMethodOverridingInspection.java
+++ python/python-psi-impl/src/com/jetbrains/python/inspections/PyMethodOverridingInspection.java
@@ -8,12 +8,9 @@ import com.intellij.psi.PsiElementVisitor;
 import com.intellij.util.containers.ContainerUtil;
 import com.jetbrains.python.PyPsiBundle;
 import com.jetbrains.python.PythonUiService;
-import com.jetbrains.python.psi.PyClass;
-import com.jetbrains.python.psi.PyFunction;
-import com.jetbrains.python.psi.PyKnownDecoratorUtil;
-import com.jetbrains.python.psi.PyUtil;
+import com.jetbrains.python.psi.*;
 import com.jetbrains.python.psi.search.PySuperMethodsSearch;
-import com.jetbrains.python.psi.types.TypeEvalContext;
+import com.jetbrains.python.psi.types.*;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -33,26 +30,50 @@ public final class PyMethodOverridingInspection extends PyInspection {
 
     @Override
     public void visitPyFunction(final @NotNull PyFunction function) {
-      final PyClass cls = function.getContainingClass();
-      if (cls == null) return;
+      final PyClass containingClass = function.getContainingClass();
+      if (containingClass == null || skipFunctionValidation(function)) return;
 
-      if (PyUtil.isConstructorLikeMethod(function) ||
-          PyKnownDecoratorUtil.hasUnknownOrChangingSignatureDecorator(function, myTypeEvalContext) ||
-          ContainerUtil.exists(PyInspectionExtension.EP_NAME.getExtensions(), e -> e.ignoreMethodParameters(function, myTypeEvalContext))) {
-        return;
+      for (PsiElement psiElement : PySuperMethodsSearch.search(function, myTypeEvalContext).findAll()) {
+        if (psiElement instanceof PyFunction baseMethod) {
+          validateOverriddenFunction(function, baseMethod, containingClass);
+        }
+      }
+    }
+
+    private boolean skipFunctionValidation(@NotNull PyFunction function) {
+      return PyUtil.isConstructorLikeMethod(function) ||
+             PyKnownDecoratorUtil.hasUnknownOrChangingSignatureDecorator(function, myTypeEvalContext) ||
+             ContainerUtil.exists(PyInspectionExtension.EP_NAME.getExtensionList(),
+                                  e -> e.ignoreMethodParameters(function, myTypeEvalContext));
+    }
+
+    private void validateOverriddenFunction(@NotNull PyFunction function,
+                                            @NotNull PyFunction baseMethod,
+                                            @NotNull PyClass containingClass) {
+      final PyClass baseClass = baseMethod.getContainingClass();
+      final String methodSignature = containingClass.getName() + "." + function.getName() + "()";
+      final String baseClassName = baseClass != null ? baseClass.getName() : "";
+
+      if (!PyUtil.isInputSignatureCompatibleTo(function, baseMethod, myTypeEvalContext)) {
+        final String msg = PyPsiBundle.message("INSP.signature.mismatch", methodSignature, baseClassName);
+
+        registerProblem(function.getParameterList(), msg,
+                        PythonUiService.getInstance().createPyChangeSignatureQuickFixForMismatchingMethods(function, baseMethod));
       }
 
-      for (PsiElement psiElement : PySuperMethodsSearch.search(function, myTypeEvalContext).asIterable()) {
-        if (psiElement instanceof PyFunction baseMethod) {
-          if (!PyUtil.isInputSignatureCompatibleTo(function, baseMethod, myTypeEvalContext)) {
-            final PyClass baseClass = baseMethod.getContainingClass();
-            final String msg = PyPsiBundle.message("INSP.signature.mismatch",
-                                                   cls.getName() + "." + function.getName() + "()",
-                                                   baseClass != null ? baseClass.getName() : "");
-            registerProblem(function.getParameterList(), msg,
-                            PythonUiService.getInstance().createPyChangeSignatureQuickFixForMismatchingMethods(function, baseMethod));
-          }
-        }
+      final PyAnnotation annotation = function.getAnnotation();
+      final PyExpression returnExpression = annotation != null ? annotation.getValue() : null;
+      if (returnExpression == null) return;
+
+      final PyType baseMethodReturnType = myTypeEvalContext.getReturnType(baseMethod);
+      final PyType overriddenReturnType = myTypeEvalContext.getReturnType(function);
+
+      final boolean isReturnTypeCompatible = baseMethodReturnType instanceof PyNeverType ||
+                                             PyTypeChecker.match(baseMethodReturnType, overriddenReturnType, myTypeEvalContext);
+
+      if (!isReturnTypeCompatible) {
+        final String msg = PyPsiBundle.message("INSP.overridden.method.return.type.mismatch", methodSignature, baseClassName);
+        registerProblem(returnExpression, msg);
       }
     }
   }
