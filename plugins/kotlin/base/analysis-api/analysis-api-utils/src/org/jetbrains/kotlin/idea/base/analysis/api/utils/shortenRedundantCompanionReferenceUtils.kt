410320e7624374475031a298e2038ca186a46b44
Roman Golyshev <roman.golyshev@jetbrains.com>
2026-02-04 16:44:28 +0100

    [kotlin] KTIJ-36810 Move code to detect and delete redundant companion references closer to `shortenUtils.kt`
    This code will be reused by the reference shortener to avoid
    touching the code on the Kotlin side, where this logic would be
    much more cumbersome to quickly implement
    
    GitOrigin-RevId: b8b00592476ce4f62ad85b209625a8ba7499b825

---
+++ plugins/kotlin/base/analysis-api/analysis-api-utils/src/org/jetbrains/kotlin/idea/base/analysis/api/utils/shortenRedundantCompanionReferenceUtils.kt
@@ -0,0 +1,81 @@
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.kotlin.idea.base.analysis.api.utils
+
+import org.jetbrains.annotations.ApiStatus
+import org.jetbrains.kotlin.analysis.api.KaSession
+import org.jetbrains.kotlin.analysis.api.analyze
+import org.jetbrains.kotlin.analysis.api.components.resolveToCall
+import org.jetbrains.kotlin.analysis.api.components.resolveToSymbol
+import org.jetbrains.kotlin.analysis.api.resolution.KaCallableMemberCall
+import org.jetbrains.kotlin.analysis.api.resolution.successfulCallOrNull
+import org.jetbrains.kotlin.analysis.api.resolution.symbol
+import org.jetbrains.kotlin.analysis.api.symbols.KaClassKind
+import org.jetbrains.kotlin.analysis.api.symbols.KaNamedClassSymbol
+import org.jetbrains.kotlin.idea.references.mainReference
+import org.jetbrains.kotlin.psi.KtDotQualifiedExpression
+import org.jetbrains.kotlin.psi.KtElement
+import org.jetbrains.kotlin.psi.KtImportDirective
+import org.jetbrains.kotlin.psi.KtNameReferenceExpression
+import org.jetbrains.kotlin.psi.KtObjectDeclaration
+import org.jetbrains.kotlin.psi.KtPsiFactory
+import org.jetbrains.kotlin.psi.KtReferenceExpression
+import org.jetbrains.kotlin.psi.psiUtil.getStrictParentOfType
+
+@ApiStatus.Internal
+fun KtReferenceExpression.canBeRedundantCompanionReference(): Boolean {
+    val element = this
+
+    val parent = element.parent as? KtDotQualifiedExpression ?: return false
+    if (parent.getStrictParentOfType<KtImportDirective>() != null) return false
+    val grandParent = parent.parent as? KtElement
+    val selectorExpression = parent.selectorExpression
+    if (element == selectorExpression && grandParent !is KtDotQualifiedExpression) return false
+    return element == selectorExpression || element.text != (selectorExpression as? KtNameReferenceExpression)?.text
+}
+
+context(_: KaSession)
+@ApiStatus.Internal
+fun KtReferenceExpression.isRedundantCompanionReference(): Boolean {
+    val parent = this.parent as? KtDotQualifiedExpression ?: return false
+
+    val referenceName = this.text
+
+    val symbol = this.mainReference.resolveToSymbol()
+    val objectDeclaration =
+        if (symbol is KaNamedClassSymbol && symbol.classKind == KaClassKind.COMPANION_OBJECT) {
+            // Try to get the PSI for the companion object
+            symbol.psi as? KtObjectDeclaration
+        } else {
+            null
+        } ?: return false
+
+    if (referenceName != objectDeclaration.name) return false
+
+    val grandParent = parent.parent as? KtElement
+    val selectorExpression = parent.selectorExpression
+
+    val (oldTargetExpression, simplifiedText) = if (grandParent is KtDotQualifiedExpression && this == selectorExpression) {
+        grandParent.selectorExpression to (parent.receiverExpression.text + "." + grandParent.selectorExpression?.text)
+    } else {
+        parent.selectorExpression to parent.selectorExpression!!.text
+    }
+
+    val oldTarget = oldTargetExpression?.resolveToCall()?.successfulCallOrNull<KaCallableMemberCall<*, *>>()?.partiallyAppliedSymbol?.symbol?.psi ?: return false
+    val fragment = KtPsiFactory(this.project).createExpressionCodeFragment(
+        simplifiedText,
+        this
+    )
+    val q = fragment.getContentElement() ?: return false
+    return oldTarget == analyze(q) {
+        val partiallyAppliedSymbol = q.resolveToCall()?.successfulCallOrNull<KaCallableMemberCall<*, *>>()?.partiallyAppliedSymbol
+        partiallyAppliedSymbol?.symbol?.psi
+    }
+}
+
+@ApiStatus.Internal
+fun KtReferenceExpression.deleteReferenceFromQualifiedExpression() {
+    val parent = this.parent as? KtDotQualifiedExpression ?: return
+    val selector = parent.selectorExpression ?: return
+    val receiver = parent.receiverExpression
+    if (this == receiver) parent.replace(selector) else parent.replace(receiver)
+}
