cb11680ea35823ed99cdbeee650bb2df6d50c376
Ilia Kirianovskii <Ilia.Kirianovskii@jetbrains.com>
2026-02-08 17:12:04 +0100

    [build] Fix JUnit 3 test suites warnings during test discovery everywhere (IJI-3451)
    GitOrigin-RevId: bef960c0761ff53e5d08e4d691fa8fa6ee744dc2

--- platform/platform-tests/testSrc/com/intellij/openapi/application/impl/ExpirableExecutorTest.kt
+++ platform/platform-tests/testSrc/com/intellij/openapi/application/impl/ExpirableExecutorTest.kt
@@ -1,7 +1,6 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.application.impl
 
-import com.intellij.openapi.Disposable
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.ExpirableExecutor
 import com.intellij.openapi.util.Disposer
@@ -31,7 +30,8 @@ class ExpirableExecutorTest : LightPlatformTestCase() {
   }
 
   fun `test coroutine canceled once expired`() {
-    val disposable = Disposable { }.also { Disposer.register(testRootDisposable, it) }
+    val disposable = Disposer.newDisposable()
+    Disposer.register(testRootDisposable, disposable)
     val context = ExpirableExecutor.on(AppExecutorUtil.getAppExecutorService())
       .expireWith(disposable)
       .coroutineDispatchingContext()
