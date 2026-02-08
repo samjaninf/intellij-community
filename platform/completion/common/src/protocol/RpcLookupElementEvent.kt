3dad1b153202653550d6a899610833b2cfac48a2
Max Medvedev <maxim.medvedev@jetbrains.com>
2026-02-08 17:04:20 +0100

    IJPL-207762 rework backendCompletionLookupMirror
    - it uses the same lookup during the whole session
    - it uses the same arranger throughout the session
    - split arrangement and item update events
    - no show event
    - don't do anything of prefix truncated
    
    GitOrigin-RevId: 0446725773fe8b2534e7bf68cbc403865585adb2

--- platform/completion/common/src/protocol/RpcLookupElementEvent.kt
+++ platform/completion/common/src/protocol/RpcLookupElementEvent.kt
@@ -1,7 +1,6 @@
 // Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.platform.completion.common.protocol
 
-import com.intellij.openapi.editor.impl.EditorId
 import com.intellij.platform.project.ProjectId
 import kotlinx.serialization.Serializable
 
@@ -16,19 +15,25 @@ sealed interface RpcLookupElementEvent {
   @Serializable
   data class CurrentItemChanged(
     val requestId: RpcCompletionRequestId,
-    val arrangementId: RpcCompletionArrangementId,
     val itemId: RpcCompletionItemId?,
-    val itemPattern: String,
-    val prefixLength: Int,
-    val additionalPrefix: String,
   ) : RpcLookupElementEvent {
     override fun toString(): String = buildToString("SelectedItem") {
       field("requestId", requestId)
-      field("arrangementId", arrangementId)
       field("itemId", itemId)
-      field("itemPattern", itemPattern)
-      field("prefixLength", prefixLength)
-      field("additionalPrefix", additionalPrefix)
+    }
+  }
+
+  /**
+   * the arrangement changed in the current lookup
+   */
+  @Serializable
+  data class ArrangementChanged(
+    val requestId: RpcCompletionRequestId,
+    val arrangementId: RpcCompletionArrangementId,
+  ) : RpcLookupElementEvent {
+    override fun toString(): String = buildToString("ArrangementChanged") {
+      field("requestId", requestId)
+      field("arrangementId", arrangementId)
     }
   }
 
@@ -52,15 +57,5 @@ sealed interface RpcLookupElementEvent {
     }
   }
 
-  @Serializable
-  /**
-   * the lookup is shown
-   */
-  data class Show(val projectId: ProjectId, val editor: EditorId) : RpcLookupElementEvent {
-    override fun toString(): String = buildToString("Show") {
-      field("projectId", projectId)
-      field("editor", editor)
-    }
-  }
 }
 
