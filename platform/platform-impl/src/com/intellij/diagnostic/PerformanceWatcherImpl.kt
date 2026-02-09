aaee90af9ff319df9f075d24a30bd5fcd23bb1e1
Aleksey Pivovarov <aleksey.pivovarov@jetbrains.com>
2026-02-09 13:54:50 +0100

    [platform] IJPL-1184 fix race with freeze dump folder rename in the PerformanceWatcher
    GitOrigin-RevId: 09a88cef99b27f9412ea66f7ad4885a3ad7c46c7

--- platform/platform-impl/src/com/intellij/diagnostic/PerformanceWatcherImpl.kt
+++ platform/platform-impl/src/com/intellij/diagnostic/PerformanceWatcherImpl.kt
@@ -2,6 +2,7 @@
 @file:Suppress("JAVA_MODULE_DOES_NOT_EXPORT_PACKAGE")
 package com.intellij.diagnostic
 
+import com.intellij.diagnostic.PerformanceWatcherImpl.MySamplingTask
 import com.intellij.featureStatistics.fusCollectors.LifecycleUsageTriggerCollector
 import com.intellij.ide.plugins.PluginManagerCore
 import com.intellij.internal.DebugAttachDetector
@@ -20,6 +21,7 @@ import com.intellij.openapi.application.ui
 import com.intellij.openapi.components.serviceAsync
 import com.intellij.openapi.diagnostic.Attachment
 import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.diagnostic.debug
 import com.intellij.openapi.diagnostic.getOrLogException
 import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.extensions.ExtensionPointName
@@ -36,6 +38,7 @@ import com.intellij.util.SystemProperties
 import com.intellij.util.concurrency.AppExecutorUtil
 import com.intellij.util.concurrency.AppScheduledExecutorService
 import com.intellij.util.concurrency.ThreadingAssertions
+import com.intellij.util.containers.ContainerUtil
 import com.intellij.util.io.basicAttributesIfExists
 import com.intellij.util.io.blockingDispatcher
 import com.intellij.util.io.sanitizeFileName
@@ -44,11 +47,13 @@ import kotlinx.coroutines.CoroutineName
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.ensureActive
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.joinAll
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import kotlinx.serialization.json.Json
@@ -421,7 +426,7 @@ internal class PerformanceWatcherImpl(private val coroutineScope: CoroutineScope
     private fun stopFreezeReporting(task: MySamplingTask) {
       val taskStop = System.nanoTime()
       coroutineScope.launch {
-        task.stopAndWait()
+        task.stop()
 
         val durationMs = TimeUnit.MILLISECONDS.convert(taskStop - taskStart, TimeUnit.NANOSECONDS)
 
@@ -445,6 +450,8 @@ internal class PerformanceWatcherImpl(private val coroutineScope: CoroutineScope
   inner class MySamplingTask(@JvmField val freezeFolder: String, private val taskStart: Long) :
     SamplingTask(dumpInterval = dumpInterval, maxDurationMs = maxDumpDuration, coroutineScope = coroutineScope) {
 
+    private val dumpTasks: MutableList<Job> = ContainerUtil.createConcurrentList()
+
     override suspend fun processDumpedThreads(infos: Array<ThreadInfo>) {
       // finish processing even after the freeze end
       val processingTask = coroutineScope.launch(CoroutineName("async freeze dumper") + blockingDispatcher) {
@@ -452,6 +459,7 @@ internal class PerformanceWatcherImpl(private val coroutineScope: CoroutineScope
         val dump = EventCountDumper.addEventCountersTo(rawDump)
         dumpedThreads(dump)
       }
+      dumpTasks += processingTask
       // don't schedule yet another thread dump - wait for completion
       processingTask.join()
     }
@@ -474,6 +482,11 @@ internal class PerformanceWatcherImpl(private val coroutineScope: CoroutineScope
       catch (e: IOException) {
         LOG.info("Failed to write the duration file", e)
       }
+    }
+
+    suspend fun waitDumpProcessing() {
+      job.join()
+      dumpTasks.joinAll()
     }
   }
 
@@ -566,10 +579,13 @@ private class CoroutineDispatcherWatcher(
   }
 }
 
-private fun postProcessReportFolder(durationMs: Long, task: SamplingTask, dir: Path, logDir: Path): Path? {
+private suspend fun postProcessReportFolder(durationMs: Long, task: MySamplingTask, dir: Path, logDir: Path): Path? {
   if (Files.notExists(dir)) {
     return null
   }
+
+  LOG.debug { "Awaiting the $dir dumping tasks to finish" }
+  task.waitDumpProcessing()
 
   cleanup(dir)
   var reportDir = logDir.resolve("${dir.name}${getFreezePlaceSuffix(task)}-${TimeUnit.MILLISECONDS.toSeconds(durationMs)}sec")
