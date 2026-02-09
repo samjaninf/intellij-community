b73ddf5b85e7d867c0d1d15209f456eb5eab2f11
Aleksey Pivovarov <aleksey.pivovarov@jetbrains.com>
2026-02-09 17:40:24 +0100

    [platform] IJPL-227644 introduce a kill-switch for coroutine thread pool exhaustion reporter
    GitOrigin-RevId: 11fd1352433112e73235625467142ef75946d9ab

--- platform/platform-impl/src/com/intellij/diagnostic/PerformanceWatcherImpl.kt
+++ platform/platform-impl/src/com/intellij/diagnostic/PerformanceWatcherImpl.kt
@@ -25,6 +25,7 @@ import com.intellij.openapi.util.SystemInfo
 import com.intellij.openapi.util.SystemInfoRt
 import com.intellij.openapi.util.io.FileUtilRt
 import com.intellij.openapi.util.io.NioFiles
+import com.intellij.openapi.util.registry.Registry
 import com.intellij.openapi.util.registry.RegistryManager
 import com.intellij.openapi.util.registry.RegistryValue
 import com.intellij.openapi.util.text.StringUtilRt
@@ -153,8 +154,10 @@ internal class PerformanceWatcherImpl(private val coroutineScope: CoroutineScope
 
     startEdtSampling()
 
-    CoroutineDispatcherWatcher(Dispatchers.Default, coroutineScope, ::pooledUnresponsiveInterval).watchDispatcher()
-    CoroutineDispatcherWatcher(Dispatchers.IO, coroutineScope, ::pooledUnresponsiveInterval).watchDispatcher()
+    if (Registry.`is`("performance.watcher.pooled.enabled")) {
+      CoroutineDispatcherWatcher(Dispatchers.Default, coroutineScope, ::pooledUnresponsiveInterval).watchDispatcher()
+      CoroutineDispatcherWatcher(Dispatchers.IO, coroutineScope, ::pooledUnresponsiveInterval).watchDispatcher()
+    }
   }
 
   private fun startEdtSampling() {
