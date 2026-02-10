05b452e26e86ba09ab62b004b796adf1e1172dad
Max Medvedev <maxim.medvedev@jetbrains.com>
2026-02-10 20:07:18 +0100

    IJPL-207762 transfer auto-popup requests from backend to frontend
    GitOrigin-RevId: 369f21b4c15922e1f44a8d11bf37dc0dc2ea21ce

--- platform/analysis-api/src/com/intellij/codeInsight/completion/NewRdCompletionSupport.kt
+++ platform/analysis-api/src/com/intellij/codeInsight/completion/NewRdCompletionSupport.kt
@@ -2,18 +2,42 @@
 package com.intellij.codeInsight.completion
 
 import com.intellij.openapi.components.service
+import com.intellij.openapi.diagnostic.logger
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.registry.Registry
 import org.jetbrains.annotations.ApiStatus
 
 @ApiStatus.Internal
 interface NewRdCompletionSupport {
   fun isFrontendRdCompletionOnImpl(): Boolean
 
+  /**
+   * Schedule autopopup for the given editor and completion type.
+   *
+   * It is expected to be called only from RD Backend.
+   */
+  fun scheduleAutopopup(project: Project, editor: Editor, completionType: CompletionType)
+
   companion object {
     @JvmStatic
     fun isFrontendRdCompletionOn(): Boolean = service<NewRdCompletionSupport>().isFrontendRdCompletionOnImpl()
+
+    @JvmStatic
+    fun reportSuboptimalUsage(message: () -> String) {
+      if (Registry.`is`("remdev.completion.on.frontend.force.errors")) {
+        logger<NewRdCompletionSupport>().error(message())
+      }
+    }
+
+    @JvmStatic
+    fun getInstance(): NewRdCompletionSupport = service<NewRdCompletionSupport>()
   }
 }
 
 internal class NoOpNewCompletionSupport : NewRdCompletionSupport {
   override fun isFrontendRdCompletionOnImpl(): Boolean = false
+
+  override fun scheduleAutopopup(project: Project, editor: Editor, completionType: CompletionType) =
+    throw UnsupportedOperationException("Is not expected to be called, use AutopopupController directly instead")
 }
