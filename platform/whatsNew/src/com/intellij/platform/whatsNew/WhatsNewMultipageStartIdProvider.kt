066f452f76bfe3f2a92f2a420e0f34e0e03d40bd
Irina Fediaeva <irina.fediaeva@jetbrains.com>
2026-02-10 15:19:20 +0100

    RIDER-132649: Return the first available multipage id if multipageIds contain only one element
    GitOrigin-RevId: 2e918ab614202085a62ec33dd064467a0a7511e4

--- platform/whatsNew/src/com/intellij/platform/whatsNew/WhatsNewMultipageStartIdProvider.kt
+++ platform/whatsNew/src/com/intellij/platform/whatsNew/WhatsNewMultipageStartIdProvider.kt
@@ -9,13 +9,19 @@ open class WhatsNewMultipageStartIdProvider(val project: Project) {
   protected open suspend fun getId(): String? = null
 
   internal suspend fun getIdIfSupported(multipageIds: List<String>): String? {
-    return getId()?.checkSupported(multipageIds)
+    if (multipageIds.size == 1) return multipageIds.first()
+    val startPageId = getId()
+    if (startPageId == null) {
+      logger.warn("What's new artifact contains several page ids but getId() returns null. Implement WhatsNewMultipageStartIdProvider.getId() to return one of them. Now trying to show the first page from available.")
+      return multipageIds.firstOrNull()
+    }
+    return startPageId.checkSupported(multipageIds)
   }
 
   private fun String.checkSupported(multipageIds: List<String>): String {
     if (this in multipageIds) return this
     else {
-      logger.warn("What's new multipage id \"$this\" is not supported")
+      logger.warn("What's new multipage id \"$this\" is not supported. Supported ids: ${multipageIds.joinToString()}")
       return this
     }
   }
