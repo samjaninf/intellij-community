5c277c03f88a5dfb38424772af83564c336d019e
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2026-02-11 14:25:56 +0100

    IJPL-233558 ModifierKeyDoubleClickHandlerTest - use junit 5
    GitOrigin-RevId: 9a1bacd1c83840389301b64870b0c63df5f5368a

--- platform/platform-tests/testSrc/com/intellij/openapi/keymap/impl/ModifierKeyDoubleClickHandlerTest.kt
+++ platform/platform-tests/testSrc/com/intellij/openapi/keymap/impl/ModifierKeyDoubleClickHandlerTest.kt
@@ -1,7 +1,7 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.keymap.impl
 
-import com.intellij.ide.IdeEventQueue.Companion.getInstance
+import com.intellij.ide.IdeEventQueue
 import com.intellij.openapi.actionSystem.ActionManager
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnAction
@@ -9,210 +9,208 @@ import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.KeyboardShortcut
 import com.intellij.openapi.keymap.KeymapManager
 import com.intellij.openapi.util.Clock
-import com.intellij.testFramework.LightPlatformTestCase
-import junit.framework.TestCase
+import com.intellij.testFramework.junit5.RunInEdt
+import com.intellij.testFramework.junit5.TestApplication
+import com.intellij.util.ui.UIUtil
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 import java.awt.event.InputEvent
 import java.awt.event.KeyEvent
 import javax.swing.JComponent
 import javax.swing.JPanel
 import javax.swing.KeyStroke
+import kotlin.test.assertEquals
 
-class ModifierKeyDoubleClickHandlerTest : LightPlatformTestCase() {
-    private val myComponent: JComponent = JPanel()
+private const val MY_SHIFT_SHIFT_ACTION = "ModifierKeyDoubleClickHandlerTest.action1"
+private const val MY_SHIFT_KEY_ACTION = "ModifierKeyDoubleClickHandlerTest.action2"
+private const val MY_SHIFT_SHIFT_KEY_ACTION = "ModifierKeyDoubleClickHandlerTest.action3"
+private const val MY_SHIFT_OTHER_KEY_ACTION = "ModifierKeyDoubleClickHandlerTest.action4"
 
-    private var myCurrentTime: Long = 0
-    private var myShiftShiftActionInvocationCount = 0
-    private var myShiftKeyActionInvocationCount = 0
-    private var myShiftShiftKeyActionInvocationCount = 0
-    private var myShiftOtherKeyActionInvocationCount = 0
+@Suppress("DEPRECATION")
+private val SHIFT_KEY_SHORTCUT = KeyboardShortcut(KeyStroke.getKeyStroke(KeyEvent.VK_BACK_SPACE, InputEvent.SHIFT_MASK), null)
+@Suppress("DEPRECATION")
+private val SHIFT_OTHER_KEY_SHORTCUT = KeyboardShortcut(KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, InputEvent.SHIFT_MASK), null)
 
-    @Throws(Exception::class)
-    public override fun setUp() {
-        super.setUp()
-        Clock.setTime(0)
-        ActionManager.getInstance().registerAction(MY_SHIFT_SHIFT_ACTION, createAction(Runnable { myShiftShiftActionInvocationCount++ }))
-        ActionManager.getInstance().registerAction(MY_SHIFT_KEY_ACTION, createAction(Runnable { myShiftKeyActionInvocationCount++ }))
-        ActionManager.getInstance()
-            .registerAction(MY_SHIFT_SHIFT_KEY_ACTION, createAction(Runnable { myShiftShiftKeyActionInvocationCount++ }))
-        ActionManager.getInstance()
-            .registerAction(MY_SHIFT_OTHER_KEY_ACTION, createAction(Runnable { myShiftOtherKeyActionInvocationCount++ }))
-        val activeKeymap = KeymapManager.getInstance().getActiveKeymap()
-        activeKeymap.addShortcut(MY_SHIFT_KEY_ACTION, SHIFT_KEY_SHORTCUT)
-        activeKeymap.addShortcut(MY_SHIFT_OTHER_KEY_ACTION, SHIFT_OTHER_KEY_SHORTCUT)
-        ModifierKeyDoubleClickHandler.getInstance().registerAction(MY_SHIFT_SHIFT_ACTION, KeyEvent.VK_SHIFT, -1)
-        ModifierKeyDoubleClickHandler.getInstance().registerAction(MY_SHIFT_SHIFT_KEY_ACTION, KeyEvent.VK_SHIFT, KeyEvent.VK_BACK_SPACE)
+private fun createAction(onPerformed: () -> Unit): AnAction {
+  return object : AnAction() {
+    override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.BGT
+
+    override fun update(e: AnActionEvent) {
+      e.presentation.setEnabledAndVisible(true)
     }
 
-    @Throws(Exception::class)
-    public override fun tearDown() {
-        try {
-            ModifierKeyDoubleClickHandler.getInstance().unregisterAction(MY_SHIFT_SHIFT_KEY_ACTION)
-            ModifierKeyDoubleClickHandler.getInstance().unregisterAction(MY_SHIFT_SHIFT_ACTION)
-            val activeKeymap = KeymapManager.getInstance().getActiveKeymap()
-            activeKeymap.removeShortcut(MY_SHIFT_OTHER_KEY_ACTION, SHIFT_OTHER_KEY_SHORTCUT)
-            activeKeymap.removeShortcut(MY_SHIFT_KEY_ACTION, SHIFT_KEY_SHORTCUT)
-            val actionManager = ActionManager.getInstance()
-            actionManager.unregisterAction(MY_SHIFT_OTHER_KEY_ACTION)
-            actionManager.unregisterAction(MY_SHIFT_SHIFT_KEY_ACTION)
-            actionManager.unregisterAction(MY_SHIFT_KEY_ACTION)
-            actionManager.unregisterAction(MY_SHIFT_SHIFT_ACTION)
-            Clock.reset()
-        } catch (e: Throwable) {
-            addSuppressedException(e)
-        } finally {
-            super.tearDown()
-        }
+    override fun actionPerformed(e: AnActionEvent) {
+      onPerformed()
+    }
+  }
+}
+
+@RunInEdt(writeIntent = true)
+@TestApplication
+@Suppress("DEPRECATION")
+class ModifierKeyDoubleClickHandlerTest {
+  private val component: JComponent = JPanel()
+
+  private var currentTime: Long = 0
+  private var shiftShiftActionInvocationCount = 0
+  private var shiftKeyActionInvocationCount = 0
+  private var shiftShiftKeyActionInvocationCount = 0
+  private var shiftOtherKeyActionInvocationCount = 0
+
+  @BeforeEach
+  fun setUp() {
+    Clock.setTime(0)
+
+    val actionManager = ActionManager.getInstance()
+    actionManager.registerAction(MY_SHIFT_SHIFT_ACTION, createAction { shiftShiftActionInvocationCount++ })
+    actionManager.registerAction(MY_SHIFT_KEY_ACTION, createAction { shiftKeyActionInvocationCount++ })
+    actionManager.registerAction(MY_SHIFT_SHIFT_KEY_ACTION, createAction { shiftShiftKeyActionInvocationCount++ })
+    actionManager.registerAction(MY_SHIFT_OTHER_KEY_ACTION, createAction { shiftOtherKeyActionInvocationCount++ })
+
+    val activeKeymap = KeymapManager.getInstance().activeKeymap
+    activeKeymap.addShortcut(MY_SHIFT_KEY_ACTION, SHIFT_KEY_SHORTCUT)
+    activeKeymap.addShortcut(MY_SHIFT_OTHER_KEY_ACTION, SHIFT_OTHER_KEY_SHORTCUT)
+
+    val doubleClickHandler = ModifierKeyDoubleClickHandler.getInstance()
+    doubleClickHandler.registerAction(MY_SHIFT_SHIFT_ACTION, KeyEvent.VK_SHIFT, -1)
+    doubleClickHandler.registerAction(MY_SHIFT_SHIFT_KEY_ACTION, KeyEvent.VK_SHIFT, KeyEvent.VK_BACK_SPACE)
+  }
+
+  @AfterEach
+  fun tearDown() {
+    val doubleClickHandler = ModifierKeyDoubleClickHandler.getInstance()
+    doubleClickHandler.unregisterAction(MY_SHIFT_SHIFT_KEY_ACTION)
+    doubleClickHandler.unregisterAction(MY_SHIFT_SHIFT_ACTION)
+
+    val activeKeymap = KeymapManager.getInstance().activeKeymap
+    activeKeymap.removeShortcut(MY_SHIFT_OTHER_KEY_ACTION, SHIFT_OTHER_KEY_SHORTCUT)
+    activeKeymap.removeShortcut(MY_SHIFT_KEY_ACTION, SHIFT_KEY_SHORTCUT)
+
+    val actionManager = ActionManager.getInstance()
+    actionManager.unregisterAction(MY_SHIFT_OTHER_KEY_ACTION)
+    actionManager.unregisterAction(MY_SHIFT_SHIFT_KEY_ACTION)
+    actionManager.unregisterAction(MY_SHIFT_KEY_ACTION)
+    actionManager.unregisterAction(MY_SHIFT_SHIFT_ACTION)
+
+    Clock.reset()
+  }
+
+  @Test
+  fun shiftShiftSuccessfulCase() {
+    press()
+    release()
+    press()
+    assertInvocationCounts(0, 0, 0)
+    release()
+    assertInvocationCounts(0, 1, 0)
+  }
+
+  @Test
+  fun longSecondClick() {
+    press()
+    release()
+    press()
+    currentTime += 400
+    Clock.setTime(currentTime)
+    release()
+    assertInvocationCounts(0, 0, 0)
+  }
+
+  @Test
+  fun shiftShiftKeySuccessfulCase() {
+    press()
+    release()
+    press()
+    key()
+    assertInvocationCounts(0, 0, 1)
+    release()
+    assertInvocationCounts(0, 0, 1)
+  }
+
+  @Test
+  fun shiftKey() {
+    press()
+    key()
+    assertInvocationCounts(1, 0, 0)
+    release()
+  }
+
+  @Test
+  fun repeatedInvocationOnKeyHold() {
+    press()
+    release()
+    press()
+    key(2)
+    assertInvocationCounts(0, 0, 2)
+    release()
+    assertInvocationCounts(0, 0, 2)
+  }
+
+  @Test
+  fun noTriggeringAfterUnrelatedAction() {
+    press()
+    release()
+    press()
+    otherKey()
+    UIUtil.dispatchAllInvocationEvents()
+    key()
+    release()
+    assertEquals(0, shiftShiftActionInvocationCount)
+    assertEquals(0, shiftShiftKeyActionInvocationCount)
+    assertEquals(1, shiftOtherKeyActionInvocationCount)
+  }
+
+  @Test
+  fun shiftShiftOtherModifierNoAction() {
+    press()
+    release()
+    press()
+    dispatchEvent(KeyEvent.KEY_PRESSED, InputEvent.SHIFT_MASK or InputEvent.CTRL_MASK, KeyEvent.VK_CONTROL, KeyEvent.CHAR_UNDEFINED)
+
+    dispatchEvent(KeyEvent.KEY_PRESSED, InputEvent.SHIFT_MASK or InputEvent.CTRL_MASK, KeyEvent.VK_BACK_SPACE, '\b')
+    dispatchEvent(KeyEvent.KEY_TYPED, InputEvent.SHIFT_MASK or InputEvent.CTRL_MASK, 0, '\b')
+    dispatchEvent(KeyEvent.KEY_RELEASED, InputEvent.SHIFT_MASK or InputEvent.CTRL_MASK, KeyEvent.VK_BACK_SPACE, '\b')
+
+    dispatchEvent(KeyEvent.KEY_RELEASED, InputEvent.SHIFT_MASK, KeyEvent.VK_CONTROL, KeyEvent.CHAR_UNDEFINED)
+    release()
+    assertInvocationCounts(0, 0, 0)
+  }
+
+  private fun assertInvocationCounts(shiftKeyCount: Int, shiftShiftCount: Int, shiftShiftKeyCount: Int) {
+    assertEquals(shiftKeyCount, shiftKeyActionInvocationCount)
+    assertEquals(shiftShiftCount, shiftShiftActionInvocationCount)
+    assertEquals(shiftShiftKeyCount, shiftShiftKeyActionInvocationCount)
+    assertEquals(0, shiftOtherKeyActionInvocationCount)
+  }
+
+  private fun press() {
+    dispatchEvent(KeyEvent.KEY_PRESSED, InputEvent.SHIFT_MASK, KeyEvent.VK_SHIFT, KeyEvent.CHAR_UNDEFINED)
+  }
+
+  private fun release() {
+    dispatchEvent(KeyEvent.KEY_RELEASED, 0, KeyEvent.VK_SHIFT, KeyEvent.CHAR_UNDEFINED)
+  }
+
+  private fun dispatchEvent(id: Int, modifiers: Int, keyCode: Int, keyChar: Char) {
+    IdeEventQueue.getInstance().dispatchEvent(
+      KeyEvent(component, id, Clock.getTime(), modifiers, keyCode, keyChar),
+    )
+  }
+
+  private fun otherKey() {
+    key(otherKey = true)
+  }
+
+  private fun key(repeat: Int = 1, otherKey: Boolean = false) {
+    val keyCode = if (otherKey) KeyEvent.VK_ENTER else KeyEvent.VK_BACK_SPACE
+    val keyChar = if (otherKey) '\n' else '\b'
+
+    repeat(repeat) {
+      dispatchEvent(KeyEvent.KEY_PRESSED, InputEvent.SHIFT_MASK, keyCode, keyChar)
+      dispatchEvent(KeyEvent.KEY_TYPED, InputEvent.SHIFT_MASK, 0, keyChar)
     }
 
-    fun testShiftShiftSuccessfulCase() {
-        press()
-        release()
-        press()
-        assertInvocationCounts(0, 0, 0, 0)
-        release()
-        assertInvocationCounts(0, 1, 0, 0)
-    }
-
-    fun testLongSecondClick() {
-        press()
-        release()
-        press()
-        timeStep(400)
-        release()
-        assertInvocationCounts(0, 0, 0, 0)
-    }
-
-    fun testShiftShiftKeySuccessfulCase() {
-        press()
-        release()
-        press()
-        key()
-        assertInvocationCounts(0, 0, 1, 0)
-        release()
-        assertInvocationCounts(0, 0, 1, 0)
-    }
-
-    fun testShiftKey() {
-        press()
-        key()
-        assertInvocationCounts(1, 0, 0, 0)
-        release()
-    }
-
-    fun testRepeatedInvocationOnKeyHold() {
-        press()
-        release()
-        press()
-        key(2)
-        assertInvocationCounts(0, 0, 2, 0)
-        release()
-        assertInvocationCounts(0, 0, 2, 0)
-    }
-
-    fun testNoTriggeringAfterUnrelatedAction() {
-        press()
-        release()
-        press()
-        otherKey()
-        key()
-        release()
-        assertInvocationCounts(1, 0, 0, 1)
-    }
-
-    fun testShiftShiftOtherModifierNoAction() {
-        press()
-        release()
-        press()
-        dispatchEvent(KeyEvent.KEY_PRESSED, InputEvent.SHIFT_MASK or InputEvent.CTRL_MASK, KeyEvent.VK_CONTROL, KeyEvent.CHAR_UNDEFINED)
-
-        dispatchEvent(KeyEvent.KEY_PRESSED, InputEvent.SHIFT_MASK or InputEvent.CTRL_MASK, KeyEvent.VK_BACK_SPACE, '\b')
-        dispatchEvent(KeyEvent.KEY_TYPED, InputEvent.SHIFT_MASK or InputEvent.CTRL_MASK, 0, '\b')
-        dispatchEvent(KeyEvent.KEY_RELEASED, InputEvent.SHIFT_MASK or InputEvent.CTRL_MASK, KeyEvent.VK_BACK_SPACE, '\b')
-
-        dispatchEvent(KeyEvent.KEY_RELEASED, InputEvent.SHIFT_MASK, KeyEvent.VK_CONTROL, KeyEvent.CHAR_UNDEFINED)
-        release()
-        assertInvocationCounts(0, 0, 0, 0)
-    }
-
-    fun assertInvocationCounts(shiftKeyCount: Int, shiftShiftCount: Int, shiftShiftKeyCount: Int, shiftOtherKeyCount: Int) {
-        TestCase.assertEquals(shiftKeyCount, myShiftKeyActionInvocationCount)
-        TestCase.assertEquals(shiftShiftCount, myShiftShiftActionInvocationCount)
-        TestCase.assertEquals(shiftShiftKeyCount, myShiftShiftKeyActionInvocationCount)
-        TestCase.assertEquals(shiftOtherKeyCount, myShiftOtherKeyActionInvocationCount)
-    }
-
-    private fun press() {
-        dispatchEvent(KeyEvent.KEY_PRESSED, InputEvent.SHIFT_MASK, KeyEvent.VK_SHIFT, KeyEvent.CHAR_UNDEFINED)
-    }
-
-    private fun release() {
-        dispatchEvent(KeyEvent.KEY_RELEASED, 0, KeyEvent.VK_SHIFT, KeyEvent.CHAR_UNDEFINED)
-    }
-
-    private fun dispatchEvent(id: Int, modifiers: Int, keyCode: Int, keyChar: Char) {
-        getInstance().dispatchEvent(
-            KeyEvent(
-                myComponent,
-                id,
-                Clock.getTime(),
-                modifiers,
-                keyCode,
-                keyChar
-            )
-        )
-    }
-
-    private fun otherKey() {
-        key(1, true)
-    }
-
-    private fun key(repeat: Int = 1, otherKey: Boolean = false) {
-        for (i in 0..<repeat) {
-            dispatchEvent(
-                KeyEvent.KEY_PRESSED, InputEvent.SHIFT_MASK,
-                if (otherKey) KeyEvent.VK_ENTER else KeyEvent.VK_BACK_SPACE,
-                if (otherKey) '\n' else '\b'
-            )
-            dispatchEvent(
-                KeyEvent.KEY_TYPED, InputEvent.SHIFT_MASK,
-                0,
-                if (otherKey) '\n' else '\b'
-            )
-        }
-        dispatchEvent(
-            KeyEvent.KEY_RELEASED, InputEvent.SHIFT_MASK,
-            if (otherKey) KeyEvent.VK_ENTER else KeyEvent.VK_BACK_SPACE,
-            if (otherKey) '\n' else '\b'
-        )
-    }
-
-    private fun timeStep(step: Long) {
-        Clock.setTime(step.let { myCurrentTime += it; myCurrentTime })
-    }
-
-    companion object {
-        private const val MY_SHIFT_SHIFT_ACTION = "ModifierKeyDoubleClickHandlerTest.action1"
-        private const val MY_SHIFT_KEY_ACTION = "ModifierKeyDoubleClickHandlerTest.action2"
-        private const val MY_SHIFT_SHIFT_KEY_ACTION = "ModifierKeyDoubleClickHandlerTest.action3"
-        private const val MY_SHIFT_OTHER_KEY_ACTION = "ModifierKeyDoubleClickHandlerTest.action4"
-
-        private val SHIFT_KEY_SHORTCUT = KeyboardShortcut(KeyStroke.getKeyStroke(KeyEvent.VK_BACK_SPACE, InputEvent.SHIFT_MASK), null)
-        private val SHIFT_OTHER_KEY_SHORTCUT = KeyboardShortcut(KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, InputEvent.SHIFT_MASK), null)
-
-        private fun createAction(runnable: Runnable): AnAction {
-            return object : AnAction() {
-                override fun getActionUpdateThread(): ActionUpdateThread {
-                    return ActionUpdateThread.BGT
-                }
-
-                override fun update(e: AnActionEvent) {
-                    e.getPresentation().setEnabledAndVisible(true)
-                }
-
-                override fun actionPerformed(e: AnActionEvent) {
-                    runnable.run()
-                }
-            }
-        }
-    }
+    dispatchEvent(KeyEvent.KEY_RELEASED, InputEvent.SHIFT_MASK, keyCode, keyChar)
+  }
 }
