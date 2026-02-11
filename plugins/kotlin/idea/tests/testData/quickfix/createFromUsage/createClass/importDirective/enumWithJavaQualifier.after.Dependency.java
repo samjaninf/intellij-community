b3e059c9b318500fe32a651e370d026407253c33
Bas Leijdekkers <bas.leijdekkers@jetbrains.com>
2026-02-11 17:21:45 +0100

    IDEA-385822 [java]: fix Override/Implement Method for empty enums
    GitOrigin-RevId: d0fe646a573c0da8cee5e577d5497b1ff7138b29

--- plugins/kotlin/idea/tests/testData/quickfix/createFromUsage/createClass/importDirective/enumWithJavaQualifier.after.Dependency.java
+++ plugins/kotlin/idea/tests/testData/quickfix/createFromUsage/createClass/importDirective/enumWithJavaQualifier.after.Dependency.java
@@ -3,6 +3,8 @@ import org.jetbrains.annotations.NotNull;
 public class J {
 
     public enum A {
+        ;
+
         @NotNull
         public static kotlin.enums.EnumEntries<A> getEntries() {
             return null;
--- plugins/kotlin/idea/tests/testData/quickfix/createFromUsage/createClass/typeReference/enumJavaTypeReceiver.after.Dependency.java
+++ plugins/kotlin/idea/tests/testData/quickfix/createFromUsage/createClass/typeReference/enumJavaTypeReceiver.after.Dependency.java
@@ -3,6 +3,8 @@ import org.jetbrains.annotations.NotNull;
 class J {
 
     public enum A {
+        ;
+
         @NotNull
         public static kotlin.enums.EnumEntries<A> getEntries() {
             return null;
