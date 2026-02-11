7cf55257b601a041c4367194583ccdceb0685b8d
Bartek Pacia <bartek.pacia@jetbrains.com>
2026-02-11 13:36:52 +0100

    docs [mod-commands]: add Javadoc to ModCommandActionQuickFixWrapper
    GitOrigin-RevId: 3b38c2375093556a0703028d121a41a7652b2a6d

--- platform/analysis-impl/src/com/intellij/lang/impl/modcommand/ModCommandActionQuickFixWrapper.java
+++ platform/analysis-impl/src/com/intellij/lang/impl/modcommand/ModCommandActionQuickFixWrapper.java
@@ -2,6 +2,7 @@
 package com.intellij.lang.impl.modcommand;
 
 import com.intellij.codeInsight.intention.preview.IntentionPreviewInfo;
+import com.intellij.codeInspection.LocalQuickFix;
 import com.intellij.codeInspection.ProblemDescriptor;
 import com.intellij.modcommand.ActionContext;
 import com.intellij.modcommand.ModCommand;
@@ -11,6 +12,7 @@ import com.intellij.openapi.diagnostic.ReportingClassSubstitutor;
 import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.NotNull;
 
+/// Wraps [ModCommandAction] so old APIs can consume it as [LocalQuickFix].
 final class ModCommandActionQuickFixWrapper extends ModCommandQuickFix implements ReportingClassSubstitutor {
   private final ModCommandAction myAction;
   private final boolean myAvailableInBatchMode;
