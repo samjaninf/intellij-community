c06469132425086a1e41aae98105fda0d08c8614
Konstantin.Hudyakov <konstantin.hudyakov@jetbrains.com>
2026-02-13 18:04:18 +0200

    [terminal] IJPL-234535 Show notification after switching terminal engine to Reworked
    GitOrigin-RevId: dbaa6f1dac22e3109d2d20597d457907e74b5eab

--- plugins/terminal/src/org/jetbrains/plugins/terminal/ExperimentalTerminalMigration.kt
+++ plugins/terminal/src/org/jetbrains/plugins/terminal/ExperimentalTerminalMigration.kt
@@ -1,9 +1,11 @@
 // Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.plugins.terminal
 
+import com.intellij.ide.util.PropertiesComponent
 import com.intellij.ide.util.RunOnceUtil
+import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.util.registry.Registry
-import com.intellij.ui.ExperimentalUI.Companion.isNewUI
+import com.intellij.ui.ExperimentalUI
 import org.jetbrains.annotations.ApiStatus
 import org.jetbrains.plugins.terminal.block.ui.updateFrontendSettingsAndSync
 
@@ -13,6 +15,8 @@ import org.jetbrains.plugins.terminal.block.ui.updateFrontendSettingsAndSync
 @ApiStatus.Internal
 object ExperimentalTerminalMigration {
   private const val EXP_ENGINE_OPTION_VISIBLE_REGISTRY = "terminal.new.ui.option.visible"
+  private const val SWITCHED_FROM_EXP_TERMINAL_PROPERTY = "terminal.switched.from.exp"
+  private const val ENGINE_SWITCH_NOTIFICATION_SHOWN_PROPERTY = "terminal.engine.switch.notification.shown"
 
   fun migrateTerminalEngineOnce(options: TerminalOptionsProvider) {
     RunOnceUtil.runOnceForApp("TerminalOptionsProvider.TerminalEngineMigration.2026.1") {
@@ -21,6 +25,8 @@ object ExperimentalTerminalMigration {
           options.terminalEngine = TerminalEngine.REWORKED
           // Ensure that the Experimental Terminal option is still visible
           Registry.get(EXP_ENGINE_OPTION_VISIBLE_REGISTRY).setValue(true)
+          PropertiesComponent.getInstance().setValue(SWITCHED_FROM_EXP_TERMINAL_PROPERTY, true)
+          thisLogger().info("Switched terminal engine to Reworked")
         }
       }
     }
@@ -31,6 +37,17 @@ object ExperimentalTerminalMigration {
    */
   fun isExpTerminalOptionVisible(): Boolean {
     TerminalOptionsProvider.instance // Ensure that all setting migrations are performed
-    return isNewUI() && Registry.`is`(EXP_ENGINE_OPTION_VISIBLE_REGISTRY, false)
+    return ExperimentalUI.isNewUI() && Registry.`is`(EXP_ENGINE_OPTION_VISIBLE_REGISTRY, false)
+  }
+
+  fun shouldShowEngineChangeNotification(): Boolean {
+    val properties = PropertiesComponent.getInstance()
+    return ExperimentalUI.isNewUI()
+           && properties.getBoolean(SWITCHED_FROM_EXP_TERMINAL_PROPERTY, false)
+           && !properties.getBoolean(ENGINE_SWITCH_NOTIFICATION_SHOWN_PROPERTY, false)
+  }
+
+  fun setEngineChangeNotificationShown() {
+    PropertiesComponent.getInstance().setValue(ENGINE_SWITCH_NOTIFICATION_SHOWN_PROPERTY, true)
   }
 }
