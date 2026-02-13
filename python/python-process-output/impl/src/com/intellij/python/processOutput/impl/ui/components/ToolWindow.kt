82f4238f40029c069cade61f812c256bd6987469
David Lysenko <david.lysenko@jetbrains.com>
2026-02-13 14:32:24 +0000

    [pycharm] PY-87577 Increase initial size of the POTW tree section
    Merge-request: IJ-MR-191384
    Merged-by: David Lysenko <david.lysenko@jetbrains.com>
    
    GitOrigin-RevId: 570fadeed15fb7591027b9ff5033f913122d45d3

--- python/python-process-output/impl/src/com/intellij/python/processOutput/impl/ui/components/ToolWindow.kt
+++ python/python-process-output/impl/src/com/intellij/python/processOutput/impl/ui/components/ToolWindow.kt
@@ -8,14 +8,19 @@ import com.intellij.python.processOutput.impl.ProcessOutputController
 import org.jetbrains.jewel.ui.component.HorizontalSplitLayout
 import org.jetbrains.jewel.ui.component.rememberSplitLayoutState
 
+internal object ToolWindowStyling {
+    val SECTION_PANE_MIN_WIDTH = 300.dp
+    const val SECTION_SPLIT_FRACTION = .25f
+}
+
 @Composable
 internal fun ToolWindow(controller: ProcessOutputController) {
     HorizontalSplitLayout(
         first = { TreeSection(controller) },
         second = { OutputSection(controller) },
         modifier = Modifier.fillMaxSize(),
-        firstPaneMinWidth = 300.dp,
-        secondPaneMinWidth = 300.dp,
-        state = rememberSplitLayoutState(.15f),
+        firstPaneMinWidth = ToolWindowStyling.SECTION_PANE_MIN_WIDTH,
+        secondPaneMinWidth = ToolWindowStyling.SECTION_PANE_MIN_WIDTH,
+        state = rememberSplitLayoutState(ToolWindowStyling.SECTION_SPLIT_FRACTION),
     )
 }
