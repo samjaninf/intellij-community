8870cfef3fc15cd20b9899385a1bea37d8822996
Morgan Bartholomew <morgan.bartholomew@jetbrains.com>
2026-02-16 03:47:20 +1000

    PY-87568 python: add inspection for coroutine in boolean context
    GitOrigin-RevId: a4225a2a8f2959947486a0bf927ff8ac388c5ccb

--- python/python-psi-api/src/com/jetbrains/python/psi/PyAssertStatement.kt
+++ python/python-psi-api/src/com/jetbrains/python/psi/PyAssertStatement.kt
@@ -1,11 +1,9 @@
 // Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.jetbrains.python.psi;
+package com.jetbrains.python.psi
+
+import com.jetbrains.python.ast.PyAstAssertStatement
 
 
-import com.jetbrains.python.ast.PyAstAssertStatement;
-import org.jetbrains.annotations.NotNull;
-
-public interface PyAssertStatement extends PyAstAssertStatement, PyStatement {
-  @Override
-  PyExpression @NotNull [] getArguments();
+interface PyAssertStatement : PyAstAssertStatement, PyStatement {
+  override val arguments: Array<PyExpression>
 }
