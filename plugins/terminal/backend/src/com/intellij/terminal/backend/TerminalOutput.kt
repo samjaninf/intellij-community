18cdcd7518c1f761ce487104675f81163106d016
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2026-02-16 15:26:28 +0200

    [terminal] IJPL-216723 Try to fix cursor blinking in apps that repaint screen intensively
    Some applications perform the following events when they need to repaint the screen:
    1. Make cursor invisible
    2. Move it and change the screen text
    3. Make cursor visible
    
    Previously we flushed changes on every cursor visibility state change. While every flush may cause screen repaint - it may have caused the blinking.
    Now we flush cursor visibility (and other not urgent things) lazily like content update events. So, in most cases this short cursor visibility change shouldn't be reflected in actual cursor rendering state.
    
    GitOrigin-RevId: da5b5206730c33bcecdab541289b976131eae70d

--- plugins/terminal/backend/src/com/intellij/terminal/backend/TerminalOutput.kt
+++ plugins/terminal/backend/src/com/intellij/terminal/backend/TerminalOutput.kt
@@ -27,8 +27,6 @@ import org.jetbrains.plugins.terminal.session.impl.TerminalOutputEvent
 import org.jetbrains.plugins.terminal.session.impl.TerminalPromptFinishedEvent
 import org.jetbrains.plugins.terminal.session.impl.TerminalPromptStartedEvent
 import org.jetbrains.plugins.terminal.session.impl.TerminalState
-import org.jetbrains.plugins.terminal.session.impl.TerminalStateChangedEvent
-import org.jetbrains.plugins.terminal.session.impl.dto.toDto
 
 @OptIn(ExperimentalCoroutinesApi::class)
 internal fun createTerminalOutputFlow(
@@ -56,6 +54,21 @@ internal fun createTerminalOutputFlow(
   val contentChangesTracker = TerminalContentChangesTracker(textBuffer, discardedHistoryTracker)
   val cursorPositionTracker = TerminalCursorPositionTracker(textBuffer, discardedHistoryTracker, terminalDisplay)
   val outputLatencyTracker = TerminalOutputLatencyTracker(services.ttyConnector, textBuffer, coroutineScope.asDisposable())
+  val stateChangesTracker = TerminalStateChangesTracker(TerminalState(
+    isCursorVisible = terminalDisplay.isCursorVisible,
+    cursorShape = terminalDisplay.cursorShape,
+    mouseMode = terminalDisplay.mouseMode,
+    mouseFormat = terminalDisplay.mouseFormat,
+    isAlternateScreenBuffer = controller.alternativeBufferEnabled,
+    isApplicationArrowKeys = controller.applicationArrowKeys,
+    isApplicationKeypad = controller.applicationKeypad,
+    isAutoNewLine = controller.isAutoNewLine,
+    isAltSendsEscape = controller.altSendsEscape,
+    isBracketedPasteMode = terminalDisplay.isBracketedPasteMode,
+    windowTitle = terminalDisplay.windowTitleText,
+    isShellIntegrationEnabled = false,
+    currentDirectory = null,
+  ))
 
   /**
    * Events should be sent in the following order: content update, cursor position update, other events.
@@ -79,7 +92,8 @@ internal fun createTerminalOutputFlow(
       else null
 
       val cursorPositionUpdate = cursorPositionTracker.getCursorPositionUpdate()
-      val updates = listOfNotNull(contentUpdateEvent, cursorPositionUpdate, otherEvent)
+      val stateUpdate = stateChangesTracker.getStateUpdate()
+      val updates = listOfNotNull(contentUpdateEvent, cursorPositionUpdate, stateUpdate, otherEvent)
       if (updates.isNotEmpty()) {
         // Block the shell output reading if any of the following:
         // 1. There are no active collectors: then there is no need to read the shell output.
@@ -108,48 +122,28 @@ internal fun createTerminalOutputFlow(
     collectAndSendEvents(contentUpdate = contentUpdate, otherEvent = null)
   }
 
-  var curState = TerminalState(
-    isCursorVisible = terminalDisplay.isCursorVisible,
-    cursorShape = terminalDisplay.cursorShape,
-    mouseMode = terminalDisplay.mouseMode,
-    mouseFormat = terminalDisplay.mouseFormat,
-    isAlternateScreenBuffer = controller.alternativeBufferEnabled,
-    isApplicationArrowKeys = controller.applicationArrowKeys,
-    isApplicationKeypad = controller.applicationKeypad,
-    isAutoNewLine = controller.isAutoNewLine,
-    isAltSendsEscape = controller.altSendsEscape,
-    isBracketedPasteMode = terminalDisplay.isBracketedPasteMode,
-    windowTitle = terminalDisplay.windowTitleText,
-    isShellIntegrationEnabled = false,
-    currentDirectory = null,
-  )
-
   controller.addListener(object : JediTerminalListener {
     override fun arrowKeysModeChanged(isApplication: Boolean) {
       textBuffer.withLock {
-        curState = curState.copy(isApplicationArrowKeys = isApplication)
-        collectAndSendEvents(contentUpdate = null, otherEvent = TerminalStateChangedEvent(curState.toDto()))
+        stateChangesTracker.updateState { it.copy(isApplicationArrowKeys = isApplication) }
       }
     }
 
     override fun keypadModeChanged(isApplication: Boolean) {
       textBuffer.withLock {
-        curState = curState.copy(isApplicationKeypad = isApplication)
-        collectAndSendEvents(contentUpdate = null, otherEvent = TerminalStateChangedEvent(curState.toDto()))
+        stateChangesTracker.updateState { it.copy(isApplicationKeypad = isApplication) }
       }
     }
 
     override fun autoNewLineChanged(isEnabled: Boolean) {
       textBuffer.withLock {
-        curState = curState.copy(isAutoNewLine = isEnabled)
-        collectAndSendEvents(contentUpdate = null, otherEvent = TerminalStateChangedEvent(curState.toDto()))
+        stateChangesTracker.updateState { it.copy(isAutoNewLine = isEnabled) }
       }
     }
 
     override fun altSendsEscapeChanged(isEnabled: Boolean) {
       textBuffer.withLock {
-        curState = curState.copy(isAltSendsEscape = isEnabled)
-        collectAndSendEvents(contentUpdate = null, otherEvent = TerminalStateChangedEvent(curState.toDto()))
+        stateChangesTracker.updateState { it.copy(isAltSendsEscape = isEnabled) }
       }
     }
 
@@ -160,58 +154,54 @@ internal fun createTerminalOutputFlow(
      */
     override fun beforeAlternateScreenBufferChanged(isEnabled: Boolean) {
       textBuffer.withLock {
-        curState = curState.copy(isAlternateScreenBuffer = isEnabled)
-        collectAndSendEvents(contentUpdate = null, otherEvent = TerminalStateChangedEvent(curState.toDto()))
+        stateChangesTracker.updateState { it.copy(isAlternateScreenBuffer = isEnabled) }
+        // Flush the changes forcefully because any other text changes will be related to the new buffer
+        collectAndSendEvents(contentUpdate = null, otherEvent = null)
       }
     }
   })
 
   terminalDisplay.addListener(object : TerminalDisplayListener {
+    override fun beep() {
+      textBuffer.withLock {
+        // Flush beep events forcefully
+        collectAndSendEvents(contentUpdate = null, otherEvent = TerminalBeepEvent)
+      }
+    }
+
     override fun cursorVisibilityChanged(isVisible: Boolean) {
       textBuffer.withLock {
-        curState = curState.copy(isCursorVisible = isVisible)
-        collectAndSendEvents(contentUpdate = null, otherEvent = TerminalStateChangedEvent(curState.toDto()))
+        stateChangesTracker.updateState { it.copy(isCursorVisible = isVisible) }
       }
     }
 
     override fun cursorShapeChanged(cursorShape: CursorShape?) {
       textBuffer.withLock {
-        curState = curState.copy(cursorShape = cursorShape)
-        collectAndSendEvents(contentUpdate = null, otherEvent = TerminalStateChangedEvent(curState.toDto()))
+        stateChangesTracker.updateState { it.copy(cursorShape = cursorShape) }
       }
     }
 
     override fun mouseModeChanged(mode: MouseMode) {
       textBuffer.withLock {
-        curState = curState.copy(mouseMode = mode)
-        collectAndSendEvents(contentUpdate = null, otherEvent = TerminalStateChangedEvent(curState.toDto()))
+        stateChangesTracker.updateState { it.copy(mouseMode = mode) }
       }
     }
 
     override fun mouseFormatChanged(format: MouseFormat) {
       textBuffer.withLock {
-        curState = curState.copy(mouseFormat = format)
-        collectAndSendEvents(contentUpdate = null, otherEvent = TerminalStateChangedEvent(curState.toDto()))
+        stateChangesTracker.updateState { it.copy(mouseFormat = format) }
       }
     }
 
     override fun bracketedPasteModeChanged(isEnabled: Boolean) {
       textBuffer.withLock {
-        curState = curState.copy(isBracketedPasteMode = isEnabled)
-        collectAndSendEvents(contentUpdate = null, otherEvent = TerminalStateChangedEvent(curState.toDto()))
+        stateChangesTracker.updateState { it.copy(isBracketedPasteMode = isEnabled) }
       }
     }
 
     override fun windowTitleChanged(title: String) {
       textBuffer.withLock {
-        curState = curState.copy(windowTitle = title)
-        collectAndSendEvents(contentUpdate = null, otherEvent = TerminalStateChangedEvent(curState.toDto()))
-      }
-    }
-
-    override fun beep() {
-      textBuffer.withLock {
-        collectAndSendEvents(contentUpdate = null, otherEvent = TerminalBeepEvent)
+        stateChangesTracker.updateState { it.copy(windowTitle = title) }
       }
     }
   })
@@ -219,8 +209,8 @@ internal fun createTerminalOutputFlow(
   shellIntegrationController.addListener(object : TerminalShellIntegrationEventsListener {
     override fun initialized(currentDirectory: String) {
       textBuffer.withLock {
-        curState = curState.copy(isShellIntegrationEnabled = true)
-        collectAndSendEvents(contentUpdate = null, otherEvent = TerminalStateChangedEvent(curState.toDto()))
+        stateChangesTracker.updateState { it.copy(isShellIntegrationEnabled = true) }
+        collectAndSendEvents(contentUpdate = null, otherEvent = null)
       }
     }
 
@@ -256,15 +246,7 @@ internal fun createTerminalOutputFlow(
     workingDirectoryTrackingScope
   ) { directory ->
     textBuffer.withLock {
-      curState = curState.copy(currentDirectory = directory)
-      collectAndSendEvents(
-        contentUpdate = null,
-        otherEvent = TerminalStateChangedEvent(curState.toDto()),
-        ensureActive = {
-          workingDirectoryTrackingScope.ensureActive()
-          ensureEmulationActive(services.terminalStarter)
-        }
-      )
+      stateChangesTracker.updateState { it.copy(currentDirectory = directory) }
     }
   }
 
