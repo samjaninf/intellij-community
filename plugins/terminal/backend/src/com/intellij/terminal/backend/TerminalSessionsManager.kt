bdd0dab523942d2778f0900cfb73c1c5c08ec7fe
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2026-02-16 12:46:50 +0100

    [terminal] IJPL-232564 support relative file path links in terminal
    GitOrigin-RevId: 83d32c79b33e1d73ebfd9fcfef4461852cc64fe7

--- plugins/terminal/backend/src/com/intellij/terminal/backend/TerminalSessionsManager.kt
+++ plugins/terminal/backend/src/com/intellij/terminal/backend/TerminalSessionsManager.kt
@@ -14,6 +14,7 @@ import org.jetbrains.plugins.terminal.JBTerminalSystemSettingsProvider
 import org.jetbrains.plugins.terminal.ShellStartupOptions
 import org.jetbrains.plugins.terminal.block.reworked.session.rpc.TerminalSessionId
 import org.jetbrains.plugins.terminal.session.impl.TerminalStartupOptionsImpl
+import org.jetbrains.plugins.terminal.util.eelDescriptor
 import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.atomic.AtomicInteger
 
@@ -57,7 +58,7 @@ class TerminalSessionsManager {
       workingDirectory = configuredOptions.workingDirectory!!,
       envVariables = configuredOptions.envVariables,
     )
-    val stateAwareSession = StateAwareTerminalSession(project, jediTermSession, options, scope)
+    val stateAwareSession = StateAwareTerminalSession(project, jediTermSession, ttyConnector.eelDescriptor, options, scope)
 
     val sessionId = storeSession(stateAwareSession, scope)
 
