16c0775f41b7210bb7c69ebf6f94b8c4f421c7c9
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2026-02-17 13:09:51 +0100

    [api] IJPL-235329 Promote ExtensionSizeLimitInfo to stable
    GitOrigin-RevId: 294bc13f24be950aa705728ab07bb7954b2e4bb8

--- platform/core-api/src/com/intellij/openapi/vfs/limits/ExtensionSizeLimitInfo.kt
+++ platform/core-api/src/com/intellij/openapi/vfs/limits/ExtensionSizeLimitInfo.kt
@@ -1,10 +1,7 @@
 // Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.vfs.limits
 
-import org.jetbrains.annotations.ApiStatus
-
 /** @see FileSizeLimit */
-@ApiStatus.Internal
 data class ExtensionSizeLimitInfo(
   /**
    * Don't load the file content if larger.
