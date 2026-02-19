3677c14f12a66cb93e05b045a15171ef119688af
Maria Sokolova <maria.sokolova@jetbrains.com>
2026-02-19 14:52:45 +0100

    IDEA-384931 [debugger]: DumpItem added mergeable token for unique items
    GitOrigin-RevId: 3f48f78c006520c1897a230ba5321c88ee2e227a

--- java/java-frontback-impl/src/com/intellij/unscramble/DumpItem.kt
+++ java/java-frontback-impl/src/com/intellij/unscramble/DumpItem.kt
@@ -104,6 +104,12 @@ interface MergeableToken {
   override fun hashCode(): Int
 
   val item: DumpItem
+
+  /** This token corresponds to a unique item which cannot be merged with others. */
+  class Unique(override val item: DumpItem) : MergeableToken {
+    override fun equals(other: Any?): Boolean = super.equals(other)
+    override fun hashCode(): Int = super.hashCode()
+  }
 }
 
 @ApiStatus.Internal
@@ -326,11 +332,7 @@ private class JavaVirtualThreadContainerItem(private val containerName: String, 
   override val awaitingDumpItems: Set<DumpItem>
     get() = emptySet()
 
-  override val mergeableToken: MergeableToken = object : MergeableToken {
-    override fun equals(other: Any?) = super.equals(other)
-    override fun hashCode() = super.hashCode()
-    override val item = this@JavaVirtualThreadContainerItem
-  }
+  override val mergeableToken: MergeableToken = MergeableToken.Unique(this)
 
   companion object {
     // see jdk.internal.vm.ThreadContainers.RootContainer.name
@@ -347,12 +349,6 @@ private class JavaVirtualThreadContainerItem(private val containerName: String, 
 }
 
 class InfoDumpItem(private val title: @Nls String, private val details: @NlsSafe String) : MergeableDumpItem {
-  override val mergeableToken: MergeableToken = object : MergeableToken {
-    override fun equals(other: Any?) = super.equals(other)
-    override fun hashCode() = super.hashCode()
-    override val item = this@InfoDumpItem
-  }
-
   override val name: @NlsSafe String
     get() = title
   override val stateDesc: @NlsSafe String
@@ -382,5 +378,7 @@ class InfoDumpItem(private val title: @Nls String, private val details: @NlsSafe
 
   override val parentTreeId: Long?
     get() = null
+
+  override val mergeableToken: MergeableToken = MergeableToken.Unique(this)
 }
 
